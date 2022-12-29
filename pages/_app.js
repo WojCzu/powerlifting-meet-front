c3e8235a20939c9669399560cadeef2754437935
WojCzu <wojciech.czuma@gmail.com>
2022-12-29 17:02:15 +0100

    feat: add global styles, variables, icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
+import "../styles/variables.css";
+import "../styles/globals.css";
+import "../styles/buttons.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }

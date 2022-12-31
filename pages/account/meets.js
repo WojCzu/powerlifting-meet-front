0c6274ce92fa1cf5e2d15d0e747c71a3f89569a8
WojCzu <wojciech.czuma@gmail.com>
2022-12-31 01:37:08 +0100

    feat: add HomePage and MyMeetsPage mocked content

--- pages/account/meets.js
+++ pages/account/meets.js
@@ -1,9 +1,40 @@
+import Link from "next/link";
 import Layout from "@/components/Layout";
+import Card from "@/components/Card";
+import AddIcon from "@/icons/add.svg";
+import Calendar from "@/icons/calendar-search.svg";
+import styles from "@/styles/MyMeets.module.css";
 
 const MyMeetsPage = () => {
   return (
     <Layout title='Moje zawody'>
-      <h1>All of my meets</h1>
+      <div className={styles.head}>
+        <h1>Moje zawody</h1>
+      </div>
+      <div>
+        <div className={styles.content}>
+          <div>
+            <div className={styles.sidebar}>
+              <div>
+                <AddIcon />
+                <span>Moje zawody:</span> 3
+              </div>
+              <div>
+                <Calendar />
+                <span>Najbliższe za:</span> 12 dni
+              </div>
+              <Link href='/meets/add' className='btn btn-black'>
+                Dodaj wydarzenie
+              </Link>
+            </div>
+          </div>
+          <div className={styles.cardList}>
+            <Card isCompact isEditable />
+            <Card isCompact isEditable />
+            <Card isCompact isEditable />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+import Link from "next/link";
 import Layout from "@/components/Layout";
+import Card from "@/components/Card";
+import styles from "@/styles/Home.module.css";
 
 const HomePage = () => {
   return (
     <Layout>
-      <h1>Few Meets</h1>
+      <div className={styles.head}>
+        <h2>Najbliższe zawody</h2>
+        <Link href={"/meets"} className='btn btn-secondary btn-black btn-sm'>
+          Zobacz wszystkie
+        </Link>
+      </div>
+      <div className={styles.cardList}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </Layout>
   );
 };

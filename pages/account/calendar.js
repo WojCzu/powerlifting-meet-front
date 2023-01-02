720a0e1365ea144e26cba571825eaecacbb20db8
WojCzu <wojciech.czuma@gmail.com>
2023-01-02 12:23:09 +0100

    feat: add calendar page mocked content

--- pages/account/calendar.js
+++ pages/account/calendar.js
@@ -1,11 +1,38 @@
 import Layout from "@/components/Layout";
+import Card from "@/components/Card";
+import BookmarkIcon from "@/icons/bookmark.svg";
+import Calendar from "@/icons/calendar-search.svg";
+import styles from "@/styles/Calendar.module.css";
 
-const MyScheduledMeetsPage = () => {
+const CalendarPage = () => {
   return (
     <Layout title='Kalendarz zawodów'>
-      <h1>Calendar</h1>
+      <div className={styles.head}>
+        <h1>Zapisane zawody</h1>
+      </div>
+      <div>
+        <div className={styles.content}>
+          <div>
+            <div className={styles.sidebar}>
+              <div>
+                <BookmarkIcon />
+                <span>Zapisanych:</span> 3
+              </div>
+              <div>
+                <Calendar />
+                <span>Najbliższe za:</span> 10 dni
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardList}>
+            <Card isCompact />
+            <Card isCompact />
+            <Card isCompact />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
 
-export default MyScheduledMeetsPage;
+export default CalendarPage;
